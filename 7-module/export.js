0593175d20ad36a3f2bd2054db41d380c76f0bbf
Ajay krishnan creations <81760087+Ajaykrishnancreations@users.noreply.github.com>
2021-11-17 11:37:01 +0530

    Add files via upload

---
+++ 7-module/export.js
@@ -0,0 +1,5 @@
+export let a=[1,2,3,4,5];
+export let b="hello world ";
+export default function Add(c,d){
+    return c+d;
+}
---
+++ 7-module/import.js
@@ -0,0 +1,7 @@
+import {a,b} from "./export.js";
+import Add from"./export.js";
+console.log(a);
+console.log(b);
+//document.write(b)
+
+console.log(Add(44,44));
